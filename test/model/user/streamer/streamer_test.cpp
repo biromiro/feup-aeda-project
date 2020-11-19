303881f3803342443076e4443208beeb21585429
EmanuelPogssoa <emanuelpessoa18@hotmail.com>
2020-11-19 17:21:53 +0000

    streamer.cpp tests done, only streamer_manager missing

--- test/model/user/streamer/streamer_test.cpp
+++ test/model/user/streamer/streamer_test.cpp
@@ -4,6 +4,7 @@
 
 #include <gtest/gtest.h>
 #include <model/user/streamer/streamer.h>
+#include <model/stream/publicStream/publicStream.h>
 
 
 TEST(streamer, constructor){
@@ -25,5 +26,13 @@ TEST(streamer, constructor_invalid_age){
 }
 
 TEST(streamer, isStreaming_setStream_removeStream){
-    Date birthdate(2001,5,16);
+    Date birthDate1(2000,11,18);
+    auto streamer = std::make_shared<Streamer>(birthDate1,"adasda adasda","ededfeqw", "sdasda");
+    EXPECT_EQ(streamer->isStreaming(),false);
+    auto stream = std::make_shared<PublicStream>("New Stream!",StreamLanguage::PT_PT,18,StreamGenre::GAMING,streamer);
+    EXPECT_EQ(streamer->isStreaming(),false);
+    streamer->setStream(stream);
+    EXPECT_EQ(streamer->isStreaming(),true);
+    streamer->removeStream();
+    EXPECT_EQ(streamer->isStreaming(),false);
 }
