b343cad6ed591441d851e3f068ea028b71b58577
Nuno Costa <up201906272@fe.up.pt>
2020-11-20 12:12:52 +0000

    more UI comments

--- src/ui/ui.h
+++ src/ui/ui.h
@@ -5,8 +5,22 @@
 #ifndef PROJECT_UI_H
 #define PROJECT_UI_H
 
+/**
+ * @file ui.h
+ *
+ * @brief Base class of all UI pages
+ *
+ * @ingroup UI
+ */
+
+/**
+ * Base class of all UI pages
+ */
 class UI{
 public:
+    /**
+     * Runs the given UI page
+     */
     virtual void run() = 0;
 };
 
--- src/ui/ui_manager.h
+++ src/ui/ui_manager.h
@@ -10,16 +10,54 @@
 #include "../auth/currentSession.h"
 #include "ui.h"
 
+/**
+ * @file ui_manager.cpp
+ *
+ * @brief Manager of the UI pages
+ *
+ * @ingroup UI
+ */
+
+/**
+ * Manager of the UI pages
+ *
+ * Allows the UI to access platform and current session information, as well as keeping track of the current UI
+ */
 class UIManager{
 public:
+
+    /**
+     * UIManager's constructor
+     *
+     * @param platform the platform (StreamZ, in this case) on which it operates
+     * @param currentSession the current User that is logged in
+     */
     UIManager(StreamZ& platform, CurrentSession& currentSession);
 
+    /**
+     * Gets the current platform on which the UIManager is operating
+     *
+     * @return the platform (StreamZ object) on which it is operating
+     */
     [[nodiscard]] StreamZ &getPlatform() const;
 
+    /**
+     * Gets the current logged user
+     *
+     * @return the currentSession object of the logged in user
+     */
     [[nodiscard]] CurrentSession &getCurrentSession() const;
 
+    /**
+     * Starts the UI
+     */
     void run() const;
 
+    /**
+     * Sets the current UI page
+     *
+     * @param ui the page to set the UI to
+     */
     void setCurrent(UI* ui);
 private:
     StreamZ& platform;
